ce08fa75cbadfb2b32ed73b062962eea1478a780
Olexandr Reshetnyk <olexandr.reshetnyk@gmail.com>
2019-09-24 12:30:22 +0300

    Was added a form with tasks list and bootstrap

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,22 +1,54 @@
-import Link from 'next/link';
-import Layout from '../comps/MyLayout';
+import React, {useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
-);
-export default function Blog() {
+
+const ListTasks = (props) => {
+  const tasks = props.tasks
+  const listItems = () => {
+    return tasks.map((task, i) => <li key={i} className="list-group-item">{task}</li>)
+  };
+
   return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
-      </ul>
-    </Layout>
-  );
+    <ul className="list-group">{listItems()}</ul>
+  )
 }
+
+const IndexPage = () => {
+  const [tasks, setTask] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  return (
+    <div className={'container'}>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if (inputValue.length > 0) {
+          setTask([...tasks, inputValue]);
+          setInputValue('')
+        }
+      }}>
+        <div className="input-group mb-3 pt-5">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Task name"
+            aria-label="Task name"
+            aria-describedby="basic-addon2"
+            value={inputValue}
+            onChange={handleChange}
+          />
+          <div className="input-group-append">
+            <input className="btn btn-outline-secondary" type="submit" value="Add"/>
+          </div>
+        </div>
+      </form>
+      <ListTasks tasks={tasks}/>
+      <style global jsx>{`
+        `}</style>
+    </div>
+  )
+}
+
+export default IndexPage
